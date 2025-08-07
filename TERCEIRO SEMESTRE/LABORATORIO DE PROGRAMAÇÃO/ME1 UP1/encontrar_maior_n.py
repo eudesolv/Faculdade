a9f999667bfb8192fe6fdcb9f18b6def4f9393e9
Eudes Oliveira <eudesorocha@gmail.com>
2025-08-07 17:14:28 -0300

    solução do exercicio encontrar o maior número

---
+++ TERCEIRO SEMESTRE/LABORATORIO DE PROGRAMAÇÃO/ME1 UP1/encontrar_maior_n.py
@@ -0,0 +1,6 @@
+def maior_numero(lista):
+    numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+    maior_numero = max(numeros)
+    return maior_numero
+
+print(maior_numero([]))
