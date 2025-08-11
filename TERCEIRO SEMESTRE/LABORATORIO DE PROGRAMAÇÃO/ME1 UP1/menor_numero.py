d9bd99953b0e577ca73a23dc70925f3b21ce827c
Eudes Oliveira <eudesorocha@gmail.com>
2025-08-11 18:54:00 -0300

    solução do exercicio de retornar o menor número

---
+++ TERCEIRO SEMESTRE/LABORATORIO DE PROGRAMAÇÃO/ME1 UP1/menor_numero.py
@@ -0,0 +1,8 @@
+def menor_numero(numero):
+  numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+
+  menor_numero = min(numeros)
+
+  return menor_numero
+
+print(menor_numero(0))
