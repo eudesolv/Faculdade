24746249cee6f2216b3fd96c7e366d58d4f63ba0
Eudes Oliveira <eudesorocha@gmail.com>
2025-08-11 18:58:33 -0300

    solução do exercicio veriricando se a lista está vazia

---
+++ TERCEIRO SEMESTRE/LABORATORIO DE PROGRAMAÇÃO/ME1 UP1/lista_vazia.py
@@ -0,0 +1,9 @@
+def lista_vazia(lista):
+
+    return len(lista) == 0
+
+print(lista_vazia([]))
+print(lista_vazia([1, 2, 3]))
+print(lista_vazia(["apple", "banana", "cherry"]))
+print(lista_vazia([True, False, True, False]))
+print(lista_vazia([]))  
